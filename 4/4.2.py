efaf02749ae13aa159e6a5ccd9134acff27d17a6
Irasov <srnan@yandex.ru>
2026-01-06 17:28:34 +0500

    feat: add solution #4.2 C

--- 4/4.2.py
+++ 4/4.2.py
@@ -5,7 +5,6 @@ def make_list(length, value=0):
 print(make_list(5))
 
 
-"""
 #Генератор матриц
 
 def make_matrix(size, value=0):
@@ -14,3 +13,14 @@ def make_matrix(size, value=0):
     else:
         n = m = size
     return [[value for _ in range(n)] for _ in range(m)]
+"""
+#Функциональный нод 2.0
+def gcd(*args):
+    a = args[0]
+    for b in args[1:]:
+        while b:
+            a, b = b, a % b
+    return a
+
+
+print(gcd(3, 102, 39, 768))
