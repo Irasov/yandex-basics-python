d9cae1b233d4a2a5ef27c71797e82ec81206d8a8
Irasov <srnan@yandex.ru>
2026-01-09 11:59:24 +0500

    feat: add solution #4.2 D,E

--- 4/4.2.py
+++ 4/4.2.py
@@ -13,7 +13,7 @@ def make_matrix(size, value=0):
     else:
         n = m = size
     return [[value for _ in range(n)] for _ in range(m)]
-"""
+
 #Функциональный нод 2.0
 def gcd(*args):
     a = args[0]
@@ -22,5 +22,44 @@ def gcd(*args):
             a, b = b, a % b
     return a
 
+    
 
-print(gcd(3, 102, 39, 768))
+#Имя of the month 2.0
+def month(number, language="ru"):
+    monts = {
+        "ru": [
+            "Январь",
+            "Февраль",
+            "Март",
+            "Апрель",
+            "Май",
+            "Июнь",
+            "Июль",
+            "Август",
+            "Сентябрь",
+            "Октябрь",
+            "Ноябрь",
+            "Декабрь",
+        ],
+        "en": [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+        ],
+    }
+    return monts[language][number - 1]
+
+
+"""
+#Подготовка данных
+def to_string(*data, sep=" ", end="\n"):
+    return sep.join(str(value) for value in data) + end
