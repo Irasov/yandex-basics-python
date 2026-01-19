62513835003607acf751bd05e8ad0bdedb2e4506
Irasov <srnan@yandex.ru>
2026-01-19 10:08:19 +0500

    feat:add solution #4.3 F

--- 4/4.3.py
+++ 4/4.3.py
@@ -43,7 +43,6 @@ print(a_plus_b(3, 5))
 print(a_plus_b(7, 9))
 
 
-"""
 #Накопление результата
 def result_accumulator(func):
     results = []
@@ -69,3 +68,31 @@ def get_letters(text: str) -> str:
 print(get_letters('Hello, world!'))
 print(get_letters('Декораторы это круто =)'))
 print(get_letters('Ехали медведи на велосипеде', method='drop'))
+
+
+"""
+#Сортировка слиянием
+def merge(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+    return merge(merge_sort(left), merge_sort(right))
+
+result = merge_sort([3, 2, 1])
+print(result)  # [1, 2, 3]
