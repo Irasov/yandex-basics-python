7ba9c4c885a11c93c424985b085d65e51cf8fe28
Irasov <srnan@yandex.ru>
2026-01-20 11:31:02 +0500

    feat: add solution #5.1 E

--- 5/5.1.py
+++ 5/5.1.py
@@ -49,7 +49,6 @@ for time in range(5):
 print(first_button.count(), second_button.count())
 
 
-"""
 #Работа не волк
 class Programmer:
 
@@ -94,3 +93,26 @@ print(programmer.info())
 programmer.rise()
 programmer.work(250)
 print(programmer.info())
+
+
+"""
+#Работа не волк
+class Rectangle:
+    def __init__(self, first, second):
+        (x1, y1), (x2, y2) = first, second
+        x1, x2 = sorted((x1, x2))
+        y1, y2 = sorted((y1, y2))
+       # self.x, self.y = x1, y1
+        self.width = x2 - x1
+        self.height = y2 - y1
+
+    def area(self):
+        return round(self.width * self.height, 2)
+
+    def perimeter(self):
+        return round(2 * (self.width + self.height), 2)
+    
+rect = Rectangle((3.2, -4.3), (7.52, 3.14))
+print(rect.perimeter())
+rect = Rectangle((7.52, -4.3), (3.2, 3.14))
+print(rect.area())
