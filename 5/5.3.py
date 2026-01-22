9d7e431fae94edeaf668e29b67261f5444cca90c
Irasov <srnan@yandex.ru>
2026-01-22 12:54:25 +0500

    feat: add .3 A

---
+++ 5/5.3.py
@@ -0,0 +1,16 @@
+"""
+"""
+#Обработка ошибок
+def func():
+    x = int('Hello, world!')
+    
+try: 
+    func()
+except ValueError:
+    print("ValueError")
+except TypeError:
+    print("TypeError")
+except SystemError:
+    print("SystemError")
+else:
+    print("No Exceptions")
