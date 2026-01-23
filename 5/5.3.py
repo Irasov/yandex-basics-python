7747b59abbb5cb90433b8b66f6ec09152f99c83d
Irasov <srnan@yandex.ru>
2026-01-23 16:14:27 +0500

    feat: add solution #5.3 B, C, D

--- 5/5.3.py
+++ 5/5.3.py
@@ -1,5 +1,4 @@
 """
-"""
 #Обработка ошибок
 def func():
     x = int('Hello, world!')
@@ -14,3 +13,37 @@ except SystemError:
     print("SystemError")
 else:
     print("No Exceptions")
+
+
+
+#Ломать — не строить
+a = None
+b = 9
+
+func(a, b)
+
+
+#Ломать — не строить 2
+class Initiator:
+    
+    def __repr__(self) -> str:
+        raise Exception
+
+
+init = Initiator()
+func(init)
+
+
+"""
+#Контроль параметров
+def only_positive_even_sum(a, b):
+    if not (isinstance(a, int) or isinstance(b, int)):
+        raise TypeError
+    if a < 0 or b < 0 or a % 2 != 0 or b % 2 != 0:
+        raise ValueError
+    return a + b
+
+print(only_positive_even_sum(3,2))
+
+        
+    
